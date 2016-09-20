742241f220d9b5610d1f7c1aec0849c3e9e2ff9a
Amy Dusek <amlydu@umich.edu>
2016-09-20 14:19:14 -0400

    Update pluralization spec.

--- spec/opinionie_spec.rb
+++ spec/opinionie_spec.rb
@@ -1,11 +1,29 @@
 require 'spec_helper'
 
+# let vs let!
+  # let:
+  #   When you have to assign a variable instead of using a before block to create an instance variable, use let.
+  #   Using let the variable lazy loads only when it is used the first time in the test and get cached until that
+  #   specific test is finished.
+  #   Initialization code only runs if the example calls it.
+  # let!:
+  #   Use let! if you want to define the variable when the block is defined.
+  #   This can be useful to populate your database to test queries or scopes.
+
 describe Opinionie::Opinion do
-  it 'does not like your opinions' do
-    expect(Opinionie::Opinion.judge('Kanye West is a God.')).to eq('Your opinion is ok. Not great. But also not good.')
+  describe :judge do
+    it 'does not like your opinions' do
+      expect(Opinionie::Opinion.judge('Kanye West is a God.')).to eq('Your opinion is ok. Not great. But also not good.')
+    end
+
+    it 'likes its own opinions' do
+      expect(Opinionie::Opinion.judge(Opinionie::Opinion.new('Watermelons are true to their name.'))).to eq('This opinion is a fountain of knowledge from which you should quench your thirst. My gift to you.')
+    end
   end
 
-  it 'likes its own opinions' do
-    expect(Opinionie::Opinion.judge(Opinionie::Opinion.new('Watermelons are true to their name.'))).to eq('This opinion is a fountain of knowledge from which you should quench your thirst. My gift to you.')
+  describe :pluralize do
+    it 'pluralizes a word' do
+      expect(Opinionie::Opinion.pluralize('opinion')).to eq('opinions')
+    end
   end
 end
